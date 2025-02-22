8250f1be51d525bce10d625956cefcc00ca70c56
truongpv03 <200033965+truongpv03@users.noreply.github.com>
2025-02-22 15:38:41 +0700

    Refactor release workflow to use npm install and Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './css-library/src/main.css',
+  entry: './src/main.css',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'css-library.min.js', // File JS không cần thiết nhưng Webpack yêu cầu
